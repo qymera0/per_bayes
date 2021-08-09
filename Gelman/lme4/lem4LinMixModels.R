5c10d5bc5acc791c8883f172abe64cbc173c963d
Samuel <samuelbaco@gmail.com>
2021-08-09 17:08:20 -0300

    lme4 section 1 + pdf

---
+++ Gelman/lme4/lem4LinMixModels.R
@@ -0,0 +1,60 @@
+
+# 0 LOAD PACKAGES ---------------------------------------------------------
+
+library(lme4)
+
+# 1 INTRODUCTION ----------------------------------------------------------
+
+## 1.1 Linear Mixed Models ------------------------------------------------
+
+
+## 1.2 Example ------------------------------------------------------------
+
+data("sleepstudy")
+
+fm1 <-
+        lmer(
+                Reaction ~ Days + (Days | Subject),
+                sleepstudy
+        )
+
+summary(fm1)
+
+RShowDoc("lmerperf", package = "lme4")
+
+
+## 1.3 High-level modular structure ---------------------------------------
+
+# Recriate fm1 as step by step
+
+
+# Parse formula
+
+parsedFormula <-
+        lFormula(
+                formula = Reaction ~ Days + (Days | Subject),
+                data = sleepstudy
+        )
+
+# Create the -log-likelihood functions
+
+devianceFunction <-
+        do.call(
+                mkLmerDevfun, parsedFormula
+        )
+
+# Run optimization
+
+optimizerOutput <-
+        optimizeLmer(
+                devianceFunction
+        )
+
+# Output
+
+mkMerMod(
+        rho = environment(devianceFunction),
+        opt = optimizerOutput,
+        reTrms = parsedFormula$reTrms,
+        fr = parsedFormula$fr
+)
